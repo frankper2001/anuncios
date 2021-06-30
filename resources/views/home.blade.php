1aa9e4d68ee6314b5179036005716c635012d15c
Frank <frankper2001.es@gmail.com>
2021-06-30 13:24:17 +0200

    fix bugs  project Anuncio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,19 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card-header">{{ __('You are logged in!') }}</div>
+                <div class="card-body">
+                    <p>Nombre: {{Auth::user()->name}}</p>
+                    <p>E-mail: {{Auth::user()->email}}</p>
+                    <p>Teléfono: {{Auth::user()->telefono}}</p>
+                    <p>Población: {{Auth::user()->poblacion}}</p>
+                </div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    @endif 
                 </div>
             </div>
         </div>
